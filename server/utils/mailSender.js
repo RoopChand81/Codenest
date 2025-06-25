89aabea42ea071a67b6779b017ce3177822e8f4e
RoopChand81 <roop46763@gmail.com>
2025-06-26 01:49:48 +0530

    implement  extra funcationality impageUploading and mailSender logic

---
+++ server/utils/mailSender.js
@@ -0,0 +1,29 @@
+//This function send a Mail on given mailAddress;
+const nodemailer= require("nodemailer");
+const mailSender =async(email,title,body)=>{
+      try{
+            //create a mail Transporter;
+            let transporter=nodemailer.createTransport({
+                  host:process.env.MAIL_HOST,// SMTP Server (e.g., Gmail, Outlook, etc.)
+                  auth:{
+                        user:process.env.MAIL_USER,// Sender Email address (from environment variable)
+                        pass:process.env.MAIL_PASS,// Email password (not your actual Gmail password)
+                  }
+            });
+         
+            //this is send the actual email on given mail Address
+            let info=await transporter.sendMail({
+                  from:'CodeNest',//The sender's name (this is displayed in the recipient's inbox)
+                  to:`${email}`,//reciver mail
+                  subject:`${title}`,//title or Subject 
+                  html:`${body}`,//body of mail formate as HTML;
+            });
+            return info;
+      }
+      catch(error){
+            console.log(error.message);
+      }
+}
+
+module.exports=mailSender;
+
